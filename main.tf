1fe625633b3f6d01cda4ea74abecf2974035b97c
Danilo Gomes <dan.theslashd@gmail.com>
2019-04-04 14:43:28 -0300

    [INC] Autoscaling; ELB;

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ variable "server_port" {
     default = 8080
 }
 
-output "public_ip" {
-    value = "${aws_instance.example.public_ip}"
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
 
 # By default, AWS does not allow any incoming or outgoing traffic from an EC2 Instance
@@ -25,14 +25,17 @@ resource "aws_security_group" "instance"{
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+    image_id = "ami-40d28157"
     instance_type = "t2.micro"
 
     #we need to tell the EC2 Instance to use it
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    security_groups = ["${aws_security_group.instance.id}"]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -40,7 +43,68 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
-    tags {
-        Name = "terraform-example"
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+data "aws_availability_zones" "all" {}
+
+resource "aws_autoscaling_group" "example"{
+    launch_configuration = "${aws_launch_configuration.example.id}"
+    availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+    # to tell the ASG to register each Instance in the ELB when that Instance is booting
+    load_balancers      = ["${aws_elb.example.name}"]
+    health_check_type    = "ELB"
+    
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+# By default, ELB don't allow any incoming or outgoing traffic (just like EC2 Instance)
+resource "aws_security_group" "elb" {
+    name = "terraform-example-elb"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Outbound requests (to allow health check requests)
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_elb" "example" {
+    name                = "terraform-asg-example"
+    availability_zones  = ["${data.aws_availability_zones.all.names}"]
+    security_groups = ["${aws_security_group.elb.id}"]
+
+    listener {
+        lb_port             = 80
+        lb_protocol         = "http"
+        instance_port       = "${var.server_port}"
+        instance_protocol   = "http"
+    }
+
+    health_check {
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        timeout             = 3
+        interval            = 30 #seconds
+        target              = "HTTP:${var.server_port}/"
     }
 }
