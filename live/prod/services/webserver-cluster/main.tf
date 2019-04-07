3a7dc89f3ee74200839ffa8b0201396ce31f5367
Danilo Gomes <dan.theslashd@gmail.com>
2019-04-07 20:17:03 -0300

    [REF] main.tf - source from github

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "../../../../modules/services/webserver-cluster"
+    source = "git::https://github.com/slackerwx/terraform_up_and_running.git//modules/services/webserver-cluster"
 
     cluster_name            = "webserver-stage"
     db_remote_state_bucket  = "terraform-up-and-running-bucket"
